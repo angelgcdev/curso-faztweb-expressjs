06c0771f4436bc1b76682367cee3dcd035884669
Luis Gutierrez <angelgcdev@gmail.com>
2024-07-25 18:04:08 -0400

    Continuar desde queries

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.text());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.get("/hello/:username", (req, res) => {
+  console.log(typeof req.params.username);
+  res.send(`Hello ${req.params.username.toUpperCase()}`);
+});
 
-app.post("/user", (req, res) => {
-  console.log(req.body);
-  res.send("Nuevo usuario creado");
+app.get("/add/:x/:y", (req, res) => {
+  const { x, y } = req.params;
+  const result = Number(x) + Number(y);
+  res.send(`Results: ${result}`);
+});
+
+app.get("/users/:username/photo", (req, res) => {
+  if (req.params.username === "angel") {
+    res.sendFile("./cat.png", { root: __dirname });
+    return;
+  }
+  res.send("El usuario no tiene acceso.");
+});
+
+app.get("/name/:name/age/:age", (req, res) => {
+  res.send(`El usuario ${req.params.name} tiene ${req.params.age} años.`);
 });
 
 app.listen(3001);
